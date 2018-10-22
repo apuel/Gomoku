3f675753a79e8646a823d7de7cbeba29adcd7220
Michael Lu <mizukori250@gmail.com>
2018-10-22 15:25:41 -0700

    The best piece removal I've ever seen, so graceful, much wow

--- src/org/us/_42/laphicet/gomoku/visualizer/Visualizer.java
+++ src/org/us/_42/laphicet/gomoku/visualizer/Visualizer.java
@@ -24,10 +24,14 @@ import java.util.List;
 import javax.imageio.ImageIO;
 
 public class Visualizer implements PlayerController, GameStateReporter {
-	public static final byte BOARD_SIZE = 19;
-	public static final byte BOARD_SPACE = 50;
-	public static final int BOARD_WIDTH = (BOARD_SIZE + 1) * BOARD_SPACE;
-	public static final int PIECE_OFFSET = 350;
+	private static final int BOARD_OFFSET = 300;
+	private static final byte BOARD_SIZE = 19;
+	private static final byte BOARD_SPACE = 50;
+	private static final int BOARD_WIDTH = (BOARD_SIZE + 1) * BOARD_SPACE;
+	private static final int PIECE_OFFSET = 150;
+	private static final int STATBOX_OFFSET1 = 100;
+	private static final int STATBOX_OFFSET2 = 250;
+	private static final int TEXTURE_OFFSET = (128/5);
 	
 	private List<Integer[]> pieces = new ArrayList<Integer[]>();
 	private int[] textures = new int[2];
@@ -48,8 +52,7 @@ public class Visualizer implements PlayerController, GameStateReporter {
 		}
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); 
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-		
-		this.window = glfwCreateWindow(BOARD_WIDTH, BOARD_WIDTH + 400, "Gomoku", NULL, NULL);
+		this.window = glfwCreateWindow(BOARD_WIDTH, BOARD_WIDTH + BOARD_OFFSET, "Gomoku", NULL, NULL);
 		if (this.window == NULL) {
 			throw new RuntimeException("Failed to create the visualizer window");
 		}
@@ -79,7 +82,7 @@ public class Visualizer implements PlayerController, GameStateReporter {
     	if (!this.mousePressed && glfwGetMouseButton(this.window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
     		glfwGetCursorPos(this.window, this.mouseX, this.mouseY);
     		coords[0] = (int)Math.round(this.mouseX.get(0)/BOARD_SPACE) - 1;
-    		coords[1] = (int)Math.round(this.mouseY.get(0)/BOARD_SPACE) - 1;
+    		coords[1] = (int)Math.round(this.mouseY.get(0)/BOARD_SPACE) - (1 + (PIECE_OFFSET / BOARD_SPACE));
     		this.mousePressed = true;
     	}
     	if (this.mousePressed && glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE) {
@@ -108,14 +111,14 @@ public class Visualizer implements PlayerController, GameStateReporter {
      * Renders a blank board on the visualizer window.
      */
     private void renderBoard() {
-    	drawLine(50, BOARD_WIDTH + 350, BOARD_WIDTH - 50, BOARD_WIDTH + 350);
-		drawLine(50, BOARD_WIDTH + 200, 50, BOARD_WIDTH + 350);
-    	drawLine(50, BOARD_WIDTH + 200, BOARD_WIDTH - 50, BOARD_WIDTH + 200);
-		drawLine(BOARD_WIDTH - 50, BOARD_WIDTH + 200, BOARD_WIDTH - 50, BOARD_WIDTH + 350);
+    	drawLine(BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET2, BOARD_WIDTH - BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET2);
+		drawLine(BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET1 , BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET2);
+    	drawLine(BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET1 , BOARD_WIDTH - BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET1 );
+		drawLine(BOARD_WIDTH - BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET1 , BOARD_WIDTH - BOARD_SPACE, BOARD_WIDTH + STATBOX_OFFSET2);
 		
     	for (float i = 1; i <= BOARD_SIZE; i++) {
-    		drawLine(BOARD_SPACE, i * BOARD_SPACE + 200, BOARD_SIZE * BOARD_SPACE, i * BOARD_SPACE + 200);
-    		drawLine(i * BOARD_SPACE, BOARD_SPACE + 200, i * BOARD_SPACE, BOARD_SIZE * BOARD_SPACE + 200);
+    		drawLine(BOARD_SPACE, (i * BOARD_SPACE) + PIECE_OFFSET, BOARD_SIZE * BOARD_SPACE, (i * BOARD_SPACE) + PIECE_OFFSET);
+    		drawLine(i * BOARD_SPACE, BOARD_SPACE + PIECE_OFFSET, i * BOARD_SPACE, (BOARD_SIZE * BOARD_SPACE) + PIECE_OFFSET);
     	}
     }
     
@@ -136,13 +139,13 @@ public class Visualizer implements PlayerController, GameStateReporter {
     	glBegin(GL_QUADS);
     	{
     		glTexCoord2f(0, 1);
-    		glVertex2i(x - (128/5), y - (128/5));
+    		glVertex2i(x - TEXTURE_OFFSET, y - TEXTURE_OFFSET);
     		glTexCoord2f(1, 1);
-    		glVertex2i(x + (128/5), y - (128/5));
+    		glVertex2i(x + TEXTURE_OFFSET, y - TEXTURE_OFFSET);
     		glTexCoord2f(1, 0);
-    		glVertex2i(x + (128/5), y + (128/5));
+    		glVertex2i(x + TEXTURE_OFFSET, y + TEXTURE_OFFSET);
     		glTexCoord2f(0, 0);
-    		glVertex2i(x - (128/5), y + (128/5));
+    		glVertex2i(x - TEXTURE_OFFSET, y + TEXTURE_OFFSET);
     	}
     	glEnd();
     	glDisable(GL_TEXTURE_2D);
@@ -200,10 +203,10 @@ public class Visualizer implements PlayerController, GameStateReporter {
 	private void beginVisualize() {
 		GL.createCapabilities();
 		
-		glMatrixMode(GL_PROJECTION);
+//		glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, BOARD_WIDTH, 0, BOARD_WIDTH + 400, -1, 1);
-        glMatrixMode(GL_MODELVIEW);
+        glOrtho(0, BOARD_WIDTH, 0, BOARD_WIDTH + BOARD_OFFSET, -1, 1);
+//        glMatrixMode(GL_MODELVIEW); 
 		
 		try {
 			this.textures[0] = initTexture("victini.png");
@@ -268,6 +271,13 @@ public class Visualizer implements PlayerController, GameStateReporter {
 		}
 		else {
 			//Remove piece matching coordinates from this.pieces
+	    	for (int i = 0; i < pieces.size(); i++) {
+	    		Integer[] tmp = pieces.get(i);
+	    		if (tmp[0] == x && tmp[1] == y) {
+	    			pieces.remove(i);
+	    			break;
+	    		}
+	    	}
 		}
 	}
 	
