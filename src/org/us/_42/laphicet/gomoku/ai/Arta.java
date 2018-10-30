e16a4647a4ca0e2ecf0e2ba36502abd83aacded3
Michael Lu <mizukori250@gmail.com>
2018-10-30 16:59:12 -0700

    Initial test for heuristics, will be adding the rest

--- src/org/us/_42/laphicet/gomoku/ai/Arta.java
+++ src/org/us/_42/laphicet/gomoku/ai/Arta.java
@@ -1,20 +1,14 @@
 package org.us._42.laphicet.gomoku.ai;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.us._42.laphicet.gomoku.Gomoku;
 import org.us._42.laphicet.gomoku.PlayerController;
-import org.us._42.laphicet.gomoku.visualizer.Piece;
 
 public class Arta implements PlayerController {
 
-	private static class Play {
+	private static class Play implements Comparable<Play> {
 		private double score;
 		private final int x;
 		private final int y;
@@ -23,10 +17,6 @@ public class Arta implements PlayerController {
 			this.score = score;
 			this.x = x;
 			this.y = y;
-		}
-		
-		private double getScore() {
-			return (this.score);
 		}
 		
 		@Override
@@ -38,24 +28,42 @@ public class Arta implements PlayerController {
 		}
 		
 		@Override
-		public int hashCode() {
-			return (Objects.hash(x, y));
+		public int compareTo(Play o) {
+			return (Double.compare(o.score, this.score));
 		}
-		
 	}
+	
+	private int playerNumber = 0;
 
 	private double[][] scoreBoard = new double[Gomoku.BOARD_LENGTH][Gomoku.BOARD_LENGTH];
-	private Set<Play> bestMoves = new HashSet<Play>();
+	private Set<Play> bestMoves = new TreeSet<Play>();
+
 	
-	private Random test = new Random();
 	
-	private double calcValue() {
-		return (test.nextInt(100));
+	
+	/**
+	 * Calculates the weight and score for heuristics
+	 * Captures weight 0.25
+	 * 
+	 * @param game
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	private double calcValue(Gomoku game, int x, int y) {
+		double score = 1;
+		score = score + ((game.countCaptures(x, y, this.playerNumber)) * 0.25);
+		return (score);
 	}
 	
-	private void scanBoard() {
+	/**
+	 * This will scan the entire board and fill it with a score. It'll than move all
+	 * the info into an TreeSet of objects where score is tied to its x and y position.
+	 */
+	private void scanBoard(Gomoku game) {
+		this.bestMoves.clear();
 		for (int x = 0, y = 0; x < Gomoku.BOARD_LENGTH && y < Gomoku.BOARD_LENGTH; x++) {
-			this.scoreBoard[x][y] = calcValue();
+			this.scoreBoard[x][y] = calcValue(game, x, y);
 			this.bestMoves.add(new Play(this.scoreBoard[x][y], x, y));
 			if (x == 18) {
 				x = -1;
@@ -71,6 +79,7 @@ public class Arta implements PlayerController {
 
 	@Override
 	public void report(Gomoku game, String message) {
+		System.err.println("[Arta] Tuturuu~");
 	}
 
 	@Override
@@ -83,12 +92,29 @@ public class Arta implements PlayerController {
 
 	@Override
 	public boolean getMove(Gomoku game, int value, long key) {
-		scanBoard();
+		if (game.getTurn() == 0) {
+			game.submitMove(9, 9, key);
+			System.out.println("0.0 9 9");
+		}
+		else {
+			scanBoard(game);
+			for (Play move : this.bestMoves) {
+				if (game.getToken(move.x, move.y) == 0 &&
+					!game.createsDoubleThree(move.x, move.y, value) &&
+					!game.isCaptured(move.x, move.y, value))
+				{
+					game.submitMove(move.x, move.y, key);
+					System.out.println(move.score + " " + move.x + " " + move.y);
+					break;
+				}
+			}
+		}
 		return (true);
 	}
 	
 	@Override
 	public void gameStart(Gomoku game, int value) {
+		this.playerNumber = value;
 	}
 	
 	@Override
